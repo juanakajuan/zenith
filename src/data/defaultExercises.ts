7cb2425b4585c7bdaa22b513ceb84d7fedc02463
Juan <juanmireles@proton.me>
2025-12-23 18:40:39 -0500

    feat: add default exercises to calves section

--- src/data/defaultExercises.ts
+++ src/data/defaultExercises.ts
@@ -1803,6 +1803,56 @@ export const DEFAULT_EXERCISES: Exercise[] = [
   },
 
   /** --- CALVES --------------------- */
+  {
+    id: "default-belt-squat-calves",
+    name: "Belt Squat Calves",
+    muscleGroup: "calves",
+    exerciseType: "machine",
+    notes: "",
+  },
+  {
+    id: "default-calf-machine",
+    name: "Calf Machine",
+    muscleGroup: "calves",
+    exerciseType: "machine",
+    notes: "Last performed 11/10/2025",
+  },
+  {
+    id: "default-leg-press-calves",
+    name: "Leg Press Calves",
+    muscleGroup: "calves",
+    exerciseType: "machine",
+    notes: "Last performed 7/24/2025",
+  },
+  {
+    id: "default-smith-machine-calves",
+    name: "Smith Machine Calves",
+    muscleGroup: "calves",
+    exerciseType: "smith-machine",
+    notes: "",
+  },
+  {
+    id: "default-stair-calves",
+    name: "Stair Calves",
+    muscleGroup: "calves",
+    exerciseType: "bodyweight",
+    notes: "Last performed 8/5/2025",
+  },
+  {
+    id: "default-stair-calves-single-leg",
+    name: "Stair Calves (Single Leg)",
+    muscleGroup: "calves",
+    exerciseType: "bodyweight",
+    notes: "",
+  },
+  {
+    id: "default-standing-calf-raise",
+    name: "Standing Calf Raise",
+    muscleGroup: "calves",
+    exerciseType: "machine",
+    notes: "",
+  },
+
   /** --- TRAPS ---------------------- */
   /** --- FOREARMS ------------------- */
   /** --- ABS ------------------------ */
