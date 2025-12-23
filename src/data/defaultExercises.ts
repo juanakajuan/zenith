8db6cf4ef5939f2e9da8bf8b7b7f533a30853eff
Juan <juanmireles@proton.me>
2025-12-23 18:44:18 -0500

    feat: add default exercises to the forearms section

--- src/data/defaultExercises.ts
+++ src/data/defaultExercises.ts
@@ -1940,5 +1940,48 @@ export const DEFAULT_EXERCISES: Exercise[] = [
   },
 
   /** --- FOREARMS ------------------- */
+  {
+    id: "default-barbell-standing-wrist-curl",
+    name: "Barbell Standing Wrist Curl",
+    muscleGroup: "forearms",
+    exerciseType: "barbell",
+    notes: "",
+  },
+  {
+    id: "default-cable-wrist-curl",
+    name: "Cable Wrist Curl",
+    muscleGroup: "forearms",
+    exerciseType: "cable",
+    notes: "Last performed 7/6/2025",
+  },
+  {
+    id: "default-dumbbell-bench-wrist-curl",
+    name: "Dumbbell Bench Wrist Curl",
+    muscleGroup: "forearms",
+    exerciseType: "dumbbell",
+    notes: "Last performed 10/9/2025",
+  },
+  {
+    id: "default-dumbbell-standing-wrist-curl",
+    name: "Dumbbell Standing Wrist Curl",
+    muscleGroup: "forearms",
+    exerciseType: "dumbbell",
+    notes: "Last performed 9/21/2025",
+  },
+  {
+    id: "default-grip-roller",
+    name: "Grip Roller",
+    muscleGroup: "forearms",
+    exerciseType: "machine",
+    notes: "",
+  },
+  {
+    id: "default-reverse-curl",
+    name: "Reverse Curl",
+    muscleGroup: "forearms",
+    exerciseType: "barbell",
+    notes: "",
+  },
+
   /** --- ABS ------------------------ */
 ];
